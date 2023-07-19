6c7a212f7cd7cbacecb1437c6c3302a5126ddc71
Shaun Harrington <81801943+sharrington92@users.noreply.github.com>
2023-07-18 17:15:41 -0700

    Started creating model

--- Midterm/03_Modeling.R
+++ Midterm/03_Modeling.R
@@ -5,24 +5,36 @@
   
   data <- read_csv("Midterm/Columbia River Flows.csv") %>% 
     mutate(
-      date = as.Date(datetime),
+      date = as.Date(datetime) %>% yearmonth(),
       year = year(date),
       month = month(date)
-    )
+    ) %>% 
+    group_by(date, site_name) %>% 
+    summarize(flow = mean(flow_cfs), gage_feet = mean(gage_feet)) %>% 
+    tsibble()
 }
 
 # Preliminary
 {
   # Use 80% for training set, 20% set
+  test <- data %>% 
+    slice_max(n = 365, order_by = date)
   
-  
+  train <- data %>% 
+    anti_join(y = test, by = "date")
 }
 
 
 # Model Estimation
 {
   # build three models + one ensemble model (average of the three forecasts) for the last year
-  
+  fit <- train %>% 
+    model(
+      "ets" = ETS(flow),
+      "arima" = ARIMA(flow),
+      "stl" = STL(flow ~ trend ),
+      "ensemble" = ets
+    )
 }
 
 
