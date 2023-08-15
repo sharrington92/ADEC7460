ea37dd39e3e6c71e678358a1b64dc739c3570586
Shaun Harrington <81801943+sharrington92@users.noreply.github.com>
2023-08-14 17:40:24 -0700

    added population

--- Final/00_Setup.R
+++ Final/00_Setup.R
@@ -207,35 +207,43 @@
         humidity_rel_avg_4w = rollmean(reanalysis_relative_humidity_percent, k = 4, na.pad = TRUE, fill = NA, align = "right"),
         humidity_rel_avg_2w = rollmean(reanalysis_relative_humidity_percent, k = 2, na.pad = TRUE, fill = NA, align = "right"),
       ) %>% 
+      ungroup() %>% 
+      left_join(
+        y = bind_rows(
+          other.data.list$Iquitos_Population_Data.csv %>%
+            mutate(city = "iq"),
+          other.data.list$San_Juan_Population_Data.csv %>% 
+            mutate(city = "sj")
+         ) %>% 
+          mutate(
+            week_start_date = paste(Year, 1, 1, sep = "-") %>%
+              ymd %>%
+              floor_date(unit = "weeks")
+          ) %>%
+          group_by(city) %>% 
+          complete(
+            week_start_date = seq.Date(from = min(week_start_date), to = max(week_start_date), by = "1 week")
+          ) %>%
+          mutate(
+            population = na.approx(Estimated_population),
+            population = rollmean(population, k = 13, fill = population, align = "center"),
+            week_start_date = yearweek(week_start_date)
+          ) %>% 
+          tsibble(key = city, index = (week_start_date)) %>%
+          select(city, week_start_date, population)
+      ) %>% 
+      group_by(city) %>% 
+      mutate(
+        case_rate = total_cases / population,
+        susc.1w = population - rollsum(total_cases, k = 1, fill = total_cases, align = "right"),
+        susc.2w = population - rollsum(total_cases, k = 2, fill = total_cases, align = "right"),
+        susc.4w = population - rollsum(total_cases, k = 4, fill = total_cases, align = "right"),
+        susc.8w = population - rollsum(total_cases, k = 8, fill = total_cases, align = "right"),
+        susc.13w = population - rollsum(total_cases, k = 13, fill = total_cases, align = "right"),
+        susc.26w = population - rollsum(total_cases, k = 26, fill = total_cases, align = "right"),
+        susc.52w = population - rollsum(total_cases, k = 52, fill = total_cases, align = "right")
+      ) %>% 
       ungroup()
-    
-    data.all.raw_with.calcs %>% 
-      as_tibble() %>% 
-      select(year, weekofyear, yearweek, week_start_date, city, days_in_week) %>% 
-      View()
-    
-    # bind_rows(
-    #   other.data.list$Iquitos_Population_Data.csv %>% 
-    #     mutate(city = "iq"),
-    #   other.data.list$San_Juan_Population_Data.csv %>% 
-    #     mutate(city = "sj")
-    # ) %>% #group_by(city) #%>% summarize(min(Year))
-    #   mutate(
-    #     week_start_date = paste(Year, 1, 1, sep = "-") %>% 
-    #       ymd %>% 
-    #       floor_date(unit = "weeks")
-    #   ) %>% 
-    #   complete(
-    #     week_start_date = seq.Date(from = ymd("1990-1-7"), to = ymd("2015-1-5"), by = "1 week"),
-    #     nesting(city)
-    #   ) %>% 
-    #   group_by(city) %>% 
-    #   mutate(
-    #     population = na.spline(Estimated_population),
-    #     week_start_date = yearweek(week_start_date)
-    #   ) %>% View()
-    #   tsibble(key = city, index = (week_start_date)) %>% 
-    #   autoplot(population)
     
     rm(data.all.raw_with.miss)
   }
@@ -408,7 +416,7 @@
 # Other Calcs ----
 {
   vars.id <- c(vars.id, "is_missing")
-  vars.y <- colnames(train.all)[str_detect(colnames(train.all), "cases")]
+  vars.y <- colnames(train.all)[str_detect(colnames(train.all), "case")]
   vars.x <- setdiff(colnames(train.all), c(vars.id, vars.y))
   vars.x.pc <- vars.x[str_detect(vars.x, "PC")]
   vars.x.sa <- vars.x[str_detect(vars.x, "_sa")]
